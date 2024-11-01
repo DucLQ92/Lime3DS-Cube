d66581078c6d2d8948b224742f0f21eeef170394
DucLQ92 <77601238+DucLQ92@users.noreply.github.com>
2024-11-01 16:44:59 +0700

    improve

--- src/android/app/src/main/java/io/github/lime3ds/android/features/settings/model/BooleanSetting.kt
+++ src/android/app/src/main/java/io/github/lime3ds/android/features/settings/model/BooleanSetting.kt
@@ -10,7 +10,7 @@ enum class BooleanSetting(
     override val defaultValue: Boolean
 ) : AbstractBooleanSetting {
     SPIRV_SHADER_GEN("spirv_shader_gen", Settings.SECTION_RENDERER, true),
-    ASYNC_SHADERS("async_shader_compilation", Settings.SECTION_RENDERER, false),
+    ASYNC_SHADERS("async_shader_compilation", Settings.SECTION_RENDERER, true),
     PLUGIN_LOADER("plugin_loader", Settings.SECTION_SYSTEM, false),
     ALLOW_PLUGIN_LOADER("allow_plugin_loader", Settings.SECTION_SYSTEM, true),
     SWAP_SCREEN("swap_screen", Settings.SECTION_LAYOUT, false),
